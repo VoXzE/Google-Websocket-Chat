ed1893eb26781f07baaa39d2d77d0307e4b3e548
VoXzE <sinncere520@gmail.com>
2019-06-23 23:11:41 -0400

    base commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import fire from './configs/firebase';
+import './assets/App.css';
+import firebase from 'firebase'
+import ChatBox from './components/ChatBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      loading: true,
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user })
+        console.log(user);
+      }
+      this.setState({loading: false })
+    });
+  }
+
+  handleSignIn = () => {
+    // Using a popup.
+    var provider = new firebase.auth.GoogleAuthProvider()
+    provider.addScope('profile');
+    provider.addScope('email');
+    fire.auth().signInWithPopup(provider).then(function (result) {
+      console.log("Logged in")
+    });
+  }
+
+  handleSignOut = () => {
+    firebase.auth().signOut().then(function () {
+      console.log('Signed Out');
+      window.location.reload();
+    }, function (error) {
+      console.error('Sign Out Error', error);
+    });
+  }
+  
+
+  render() {
+    const { user, loading } = this.state;
+
+    if (loading) {
+      return (
+        <h1>Loading...</h1>
+      )
+    }
+    return (
+      <div className="App">
+        {/* {user && user.displayName} */}
+        {user && <ChatBox name={user.displayName}/>}
+        <button onClick={this.handleSignIn}>Sign In</button>
+        <button onClick={this.handleSignOut}>Sign Out</button>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/ChatBox.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import TextField from '@material-ui/core/TextField';
+import io from 'socket.io-client';
+
+export default class ChatBox extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: null,
+      messages: []
+    }
+
+    this.socket = io('localhost:8080'); 
+    
+    this.socket.on("RECIEVE_CHAT", (data) => {
+      addChatMessage(data);
+    });
+
+    const addChatMessage = (data) => {
+      this.setState({ messages: [...this.state.messages, data]});
+      console.log(this.state.messages);
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ message: event.target.value })
+  }
+
+  handleSendMessage = (event) => {
+    event.preventDefault();
+    this.socket.emit("SEND_CHAT", {
+      name: this.props.name,
+      text: this.state.message,
+    });
+    this.setState({ message: null })
+  }
+
+ 
+
+  render() {
+    return (
+      <div className="chat-box">
+        <div className="chat-messages">
+          {this.state.messages && this.state.messages.map((message) => (
+            <div className="chat-message">
+              <h1>{message.name}</h1>
+              <p>{message.text}</p>
+            </div>
+          ))}
+        </div>
+        <form noValidate autoComplete="off" className="chat-input" onSubmit={this.handleSendMessage}>
+          <TextField
+            label="Enter chat message"
+            className="chat-text"
+            onChange={this.handleChange}
+            value={this.state.message}
+            margin="normal"
+          />
+        </form>
+      </div>
+    )
+  }
+}
